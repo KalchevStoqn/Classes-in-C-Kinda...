4b469226cdb6b83832fde1d059acb7cac5b19632
KalchevStoqn <diniturdimi@gmail.com>
2026-05-16 21:14:33 +0300

    Add files via upload

---
+++ ConsoleApp1/Devil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Devil
+    {
+        // Properties of a Devil! (Also means "what your class has.For example a devil has a name,
+        // Horns,idk why i put color here but also a color, and definitely aura.  ")
+
+        public bool Horns { get; set; }
+        public string Colour { get; set; }
+        public string Name { get; set; }
+        public int Aura { get; set; }
+        public bool ShootFireBalls { get;  private set; }  // 'private set' means it can only be changed inside this class
+
+        // 2. Constructor (The blueprint's setup instructions)
+
+        public Devil(bool horns, string colour, string name, int aura) 
+        {
+
+            Horns = horns;
+            Colour = colour;
+            Name = name;
+            Aura = aura;
+
+        }
+        
+
+        // 3. Methods ( think of it as like actions a devil can perform.)
+
+        public void Destruction(int fireballPower)
+        {
+            fireballPower = 10000000;
+            ShootFireBalls = true;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ConsoleApp1;
+
+class program 
+{
+ 
+
+    static void Main(string[] args)
+    {
+        //  okay first: Create a new Car object using the constructor. And add all the things you typed in!
+
+        Devil devil = new Devil(true, "red", "Dante", 1000000);
+
+        // now second:  Access properties
+
+        Console.WriteLine($"the {devil.Colour} devil has {devil.Aura}  aura, and his name is {devil.Name} ,and the devil has {devil.Horns}!");
+
+        // third: You call your methods!
+        devil.Destruction(1000000);
+        
+    }
+}
+
